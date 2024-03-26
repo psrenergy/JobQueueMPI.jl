cb055c401c2df687e39e42054ce69099d5e23271
pedroripper <pedros.ripper@gmail.com>
2024-03-26 12:04:59 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,10 @@ ENV["JULIA_PROJECT"] = dirname(Base.active_project())
 
 JQM = JobQueueMPI
 
+test_1_path = joinpath(".", "test1.jl")
+test_2_path = joinpath(".", "test2.jl") 
+
 @testset verbose = true "JobQueueMPI Tests" begin
-    mpiexec(exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project .\\test1.jl`))
-    mpiexec(exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project .\\test2.jl`))
+    mpiexec(exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project $(test_1_path)`))
+    mpiexec(exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project $(test_2_path)`))
 end
